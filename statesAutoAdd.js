cd44afedcb91b94fa97a3568e930fe877b67ef95
marvel6 <marvelloussolomon49@gmail.com>
2023-03-18 11:57:33 +0100

    new commit

--- statesAutoAdd.js
+++ statesAutoAdd.js
@@ -7,7 +7,7 @@ const jsonFile = require('./data.json')
 const start = async () => {
     try {
         await connectDb('mongodb://0.0.0.0:27017/AUTH_NODE')
-        await State.deleteMany()
+        // await State.deleteMany()
         await State.create(jsonFile)
         console.log('success added the files')
         process.exit(0)
